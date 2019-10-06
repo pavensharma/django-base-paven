658d897fa34f43eeb6eb1cd7559d969f5689ac03
PavenSharma <pavensharma90@gmail.com>
2019-10-06 16:04:55 +0800

    example of basic input output and bootstrap applied

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def x(request):
+    fulltext = request.GET['fulltext']
+
+    return render(request,'output.html', {'y':fulltext})
--- django_base/urls.py
+++ django_base/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
 
 urlpatterns = [
+    #path('my_app_1/', include('my_app_1.urls')),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
